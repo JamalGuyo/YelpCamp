eedb521da808d363b206e999f6a703a20e99960f
JamalGuyo <jamalguyo08@gmail.com>
2020-04-08 21:25:21 +0300

    create campgrounds routes and templates

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
   res.render("landing");
 });
 
+app.get("/campgrounds", (req, res) => {
+  const campgrounds = [
+    { name: "Campsite1", img: "image file url" },
+    { name: "Campsite2", img: "image file url" },
+    { name: "Campsite3", img: "image file url" },
+    { name: "Campsite4", img: "image file url" },
+    { name: "Campsite5", img: "image file url" },
+    { name: "Campsite6", img: "image file url" },
+  ];
+  res.render("campgrounds", { campgrounds });
+});
+
 // listener
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
