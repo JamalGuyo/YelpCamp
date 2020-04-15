e691fcf2c67daf58fe25fb3ddf07408cea6cc6a5
JamalGuyo <jamalguyo08@gmail.com>
2020-04-15 21:10:48 +0300

    link user to comment

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 // schema
 const commentSchema = new mongoose.Schema({
   text: String,
-  author: String,
+  author: [
+    {
+      id:
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      username: String
+    }
+  ],
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
