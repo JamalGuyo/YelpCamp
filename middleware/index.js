d58e3e2584f27e9dfcdf02553b008f67c49a88f9
JamalGuyo <jamalguyo08@gmail.com>
2020-04-17 14:56:00 +0300

    add flash messages in middleware

--- middleware/index.js
+++ middleware/index.js
@@ -7,16 +7,19 @@ middleware.checkCampgroundOwnership = (req, res, next) => {
     if (req.isAuthenticated()) {
         Campground.findById(req.params.id, (err, campground) => {
             if (err) {
+                req.flash('error', 'Campground not found!');
                 res.redirect('back');
             } else {
                 if (campground.author.id.equals(req.user._id)) {
                     next();
                 } else {
+                    req.flash('error', 'You don\'t have the permission to do that');
                     res.redirect('back');
                 }
             }
         })
     } else {
+        req.flash('error', 'You need to be logged in to do that');
         res.redirect('back');
     }
 }
@@ -25,16 +28,19 @@ middleware.checkCommentOwnership = (req, res, next) => {
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, (err, comment) => {
             if (err) {
+                req.flash('error', 'Comment not found!');
                 res.redirect('back');
             } else {
                 if (comment.author.id.equals(req.user._id)) {
                     next();
                 } else {
+                    req.flash('error', 'You don\'t have the permission to do that');
                     res.redirect('back');
                 }
             }
         })
     } else {
+        req.flash('error', 'You need to be logged in to do that');
         res.redirect('back');
     }
 }
@@ -43,6 +49,7 @@ middleware.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash('error', 'You need to be logged in to do that');
     res.redirect('/login');
 }
 
